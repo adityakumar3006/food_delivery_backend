b9b83f9efb0229791a9adf1978ee0a5f12565481
Aditya kumar <aditya.kumar3006@gmail.com>
2024-05-03 15:29:55 +0530

    fourth commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mongoDB = require("./db");
 
 mongoDB();
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://dynamic-pixie-25914d.netlify.app/"); // Use FRONTEND_URL environment variable
+    res.setHeader("Access-Control-Allow-Origin", "https://dynamic-pixie-25914d.netlify.app"); // Use FRONTEND_URL environment variable
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
